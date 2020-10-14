353bf3a4057db8bab9543ff6d8e20752dc3d694e
PHOENIXTechVault <72236752+PHOENIXTechVault@users.noreply.github.com>
2020-10-14 13:42:26 +0100

    Add files via upload

---
+++ basic_terms.py
@@ -0,0 +1,25 @@
+a = 2,417,9,-7 #intergers all positive and negative whole numbers
+b = 1.5,-78.1,9.9 #float decimal or fractional numbers.
+c = "cat",'bag',"dog" #strings these are sequence of characters.
+
+print(type(a))  #checking the type of each value stored using the type() function
+print(type(b))  #checking the type of each value stored using the type() function
+print(type(c))  #checking the type of each value stored using the type() function
+
+                #NB: MAKE SURE TO STORE JUST ONE VALUE TO AVOID GETTING DIFFERENT RESULTS
+
+exponent = **   #Also known as powers, denoted by two asterisks
+division = /    #float division
+multiplication = * # multiplication
+sub = - #substraction
+add = + #addition
+int div = //  #floor or integer division, this will always return an int data type
+
+modulus = %  #modulus , returns the remainder in a division
+
+
+print('ama ' + 'is going to campus')  #combining two strings using the + operator
+                                        #this method is called concatenation
+
+print('ama '*3)     # this method is called string replication. this repeat the string value a given number of times.
+
