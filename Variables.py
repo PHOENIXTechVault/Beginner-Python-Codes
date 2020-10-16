6bc2bcf711a00c2f1809b58b7e798a36c564f96c
PHOENIXTechVault <72236752+PHOENIXTechVault@users.noreply.github.com>
2020-10-16 12:13:00 +0100

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,43 @@
+name = "Joe" #creating a variable called name and storing the value Joe inside it
+
+action = "walking" #creating another variable called action and storing the value walking inside it
+
+print(name + " is " + action) #printing out a sentence using the variables and to the strings directly
+
+employee_1 = "Juliet" #eg2. creating a variable called employee_1 and storing the value Juliet inside it
+
+pay_of_employee1 = '5000' #creating a variable called pay_of_employee1 and storing the value 5000 inside it
+
+
+employee_2 = "John" #creating a variable called employee_2 and storing the value John inside it
+
+pay_of_employee2 = '2500' #creating a variable called pay_of_employee2 and storing the value 2500 inside it
+
+print(employee_1 + " receives " + pay_of_employee1 + " while " + employee_2 + " receives " + pay_of_employee2) # printing out the values of the variables together with some strings to form a sentence
+
+num1 = 10 #creating a variable called num1 and storing the value 10 inside it
+
+num2 = 5 #creating a variable called num2 and storing the value 5 inside it
+
+print(num1**num2) #performing mathematical operations with variables
+
+total = num2 + num1 #you can also store the results obtained from variables inside another variable
+
+print(total) #printing out the value or final answer results
+
+amount = 1000 #creating a variable called amount and storing the value 1000 inside it
+print(amount) #printing out the value inside the variable amount
+amount = 50  #re-assigning new value to the same variable
+print(amount)  #printing out the new value inside the variable
+
+words = "Hello come and sleep" #creating a variable called words and storing the value Hello come and sleep inside it
+print(words)  #printing out the value stored in the variable words
+
+words = "Hello this is the newly assigned value" #re-assigning new value to the same variable
+print(words)  #printing out the new value stored in the variable words
+print(words)  #printing out the new value stored in the variable words
+print(words)  #printing out the new value stored in the variable words
+print(words)  #printing out the new value stored in the variable words
+
+words = "Go and eat"  #again re-assigning new value to the same variable
+print(words)  #printing out the new value inside the same variable
