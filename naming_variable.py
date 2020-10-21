ed93d5d1566428d6bdfe59cd6ea5887155818861
PHOENIXTechVault <72236752+PHOENIXTechVault@users.noreply.github.com>
2020-10-21 04:15:51 +0100

    Add files via upload

---
+++ naming_variable.py
@@ -0,0 +1,22 @@
+age = 45 #variable names must be meaningful and descriptive of the value it is contains
+
+#1age = 13 #invalid variable name, cannot begin variable names with numbers
+
+#colors@ = "blue" #invalid variable name, cannot use special characters in naming variables
+                    #apart from _ all special characters are not allowed when naming variables
+
+#for = 7    #invalid variable name, cannot use Python keywords in naming variables
+
+#return = "stay" #invalid variable name, cannot use Python keywords in naming variables
+
+AGE = 15 #Altough accepted but not recommended since it might cause mistakes
+age = 15 #always recommended to keep your variable names in lowercase
+
+
+
+#age of student = 14 #invalid variable name, cannot create whitespace in variable names
+
+                #It's recommended to use underscores in place of the whitespaces
+                #e.g. age_of_student , this is accepted and won't get an error when executed
+
+#print(age of student) #this will give an error when file is executed
