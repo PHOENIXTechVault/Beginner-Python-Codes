1b267f49c19993e857c5ee7f5c9d68bf8ac78a3d
PHOENIXTechVault <72236752+PHOENIXTechVault@users.noreply.github.com>
2020-11-04 03:30:06 +0000

    Add files via upload

---
+++ manipulating-strings.py
@@ -0,0 +1,28 @@
+a = 'How's it going' #this will give an error
+print(a)
+
+b = "He said,"shut up and sit"and he left"    #this will give an error
+print(b)
+
+#using the double quotes and single quotes interchangably
+a = "How's it going"
+print(a)
+
+b = 'He said,"shut up and sit"and he left'
+print(b)
+
+
+#using the raw string method
+
+word = r'''he's gone'''
+print(word)
+
+sentence = r"""she said, "come and lets it's your's go"""
+print(sentence)
+
+#using the escape characters \ \\ \t \n \b \a ...etc
+
+c = 'How\\s it \ngoing'
+print(c)
+
+
