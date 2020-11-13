24851a6cb36b6c2779e609bf3993dea108b726b8
PHOENIXTechVault <72236752+PHOENIXTechVault@users.noreply.github.com>
2020-11-13 04:38:04 +0000

    Add files via upload

---
+++ string-methods-I.py
@@ -0,0 +1,22 @@
+ # upper()  transforms the string to uppercase
+a = "Python"
+
+b = a.upper()
+# print(a ,b)
+
+#  lower()  transforms the string to lowercase
+c = "PHOENIX"
+
+print(c.lower())
+
+# isupper()  returns True if the string is in uppercase and False if otherwise
+v = "PHOENIX"
+
+print(v.isupper())
+
+# islower()     returns True if the string is in lowercase and False if otherwise
+
+n = "webdev"
+
+p = n.islower()
+print(p)
