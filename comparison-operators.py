5cbaa34b7bfca5d68a6568c9a12ad8266833796d
PHOENIXTechVault <72236752+PHOENIXTechVault@users.noreply.github.com>
2020-11-20 02:39:09 +0000

    Add files via upload

---
+++ comparison-operators.py
@@ -0,0 +1,43 @@
+# ==        returns True if the two operands are equal
+a = 4
+b = 4
+c = 89
+d = 89
+s = "4"
+
+print(a == s)
+
+print(type(a))
+
+print(type(s))
+print(a == b)
+print(a == c)
+
+# !=    returns True if the two operands are not equal
+
+print(a != s)
+
+print(a != c)
+print(a != b)
+
+# <     returns True if the left operand is less than the right operand
+print(a < s)
+print(a < c)
+
+print(c < a)
+
+# >     returns True if the left operand is greater than the right operand
+
+print(c > a)
+
+print(a > c)
+
+# <=    returns True if the left operand is smaller than or equal to the right operand
+
+print(a <= b)
+
+# >=    returns True if the left operand is greater than or equal to the right operand
+
+print(c >= a)
+
+print(c >= d)
