758b0f0457a47680b3e4de570879d252c39e4710
PHOENIXTechVault <72236752+PHOENIXTechVault@users.noreply.github.com>
2020-11-27 03:11:44 +0000

    Add files via upload

---
+++ conditional-statement.py
@@ -0,0 +1,18 @@
+name = "Mary"
+sex = "male"
+age = 12
+
+# if
+if name == "Sam":  #the structure consist of the if keyword, the specified condition, a colon, and an indented block of code
+    # indented block of code
+    print("Welcome " + name)
+# elif
+elif sex == "Female":   #the structure consist of the elif keyword, the specified condition, a colon, and an indented block of code
+    # indented block of code
+    print("You are a female")
+elif age < 18:
+    print("You are not 18 years old")
+# else
+else:   #the structure consist of the else keyword, a colon, and an indented block of code
+    # indented block of code
+    print("You are not welcomed")
